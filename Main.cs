307a78b1ce135baeb56a6cf4701dd1d6e4e26666
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-07-02 15:20:37 +0800

    Removed KitchenLib dependency

--- Main.cs
+++ Main.cs
@@ -1,36 +1,30 @@
-﻿using KitchenLib;
-using KitchenMods;
-using System.Reflection;
+﻿using KitchenMods;
 using UnityEngine;
 
 // Namespace should have "Kitchen" in the beginning
 namespace KitchenSmartGrabbersWereBugged
 {
-    public class Main : BaseMod, IModSystem
+    public class Main : IModInitializer
     {
         // guid must be unique and is recommended to be in reverse domain name notation
         // mod name that is displayed to the player and listed in the mods menu
         // mod version must follow semver e.g. "1.2.3"
         public const string MOD_GUID = "IcedMilo.PlateUp.SmartGrabbersWereBugged";
         public const string MOD_NAME = "Smart Grabbers Were Bugged";
-        public const string MOD_VERSION = "0.1.3";
-        public const string MOD_AUTHOR = "IcedMilo";
-        public const string MOD_GAMEVERSION = ">=1.1.1";
-        // Game version this mod is designed for in semver
-        // e.g. ">=1.1.1" current and all future
-        // e.g. ">=1.1.1 <=1.2.3" for all from/until
+        public const string MOD_VERSION = "0.1.4";
 
-        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+        public Main()
+        {
+        }
 
-        protected override void OnPostActivate(Mod mod)
+        public void PostActivate(Mod mod)
         {
             LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
         }
 
-        protected override void OnUpdate()
-        {
-            
-        }
+        public void PreInject() { }
+
+        public void PostInject() { }
 
         #region Logging
         // You can remove this, I just prefer a more standardized logging
